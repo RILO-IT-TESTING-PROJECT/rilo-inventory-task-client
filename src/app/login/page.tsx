221952c68879b0e8245c70c6a75359266ebe26e0
FM-Akib <muntasirakib08@gmail.com>
2024-12-05 14:29:32 +0600

    Chore(Feature-login and register): login and register form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,105 @@
+"use client"
+import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import Link from 'next/link';
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters.",
+  }),
+})
+
+const LoginPage = () => {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data)
+  }
+
+  return (
+    <section className="flex items-center justify-center w-full min-h-screen bg-gray-100">
+      <div className="w-full max-w-md px-8 py-8 bg-gray-200 rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold  mb-6">Login</h2>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            {/* Username Field */}
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input 
+                    className='bg-white text-gray-800' 
+                    placeholder="Enter your username" 
+                    {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    This is your public display name.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Password Field */}
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      className='bg-white text-gray-800'
+                      type="password"  // Set password field type to "password" to hide characters
+                      placeholder="Enter your password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Enter a strong password to protect your account.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* Submit Button */}
+            <Button type="submit" className="w-full">Submit</Button>
+          </form>
+        </Form>
+        <div className="mt-4  text-sm">
+          Don&apos;t have an account?{" "}
+          <Link href="/register" className="text-primary hover:underline">
+            Register
+          </Link>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default LoginPage;
